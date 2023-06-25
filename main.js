04b6eb0d446da2c0b8efeaef749572d5eaea8bc6
yolovi <yolandalopevi@gmail.com>
2023-06-25 19:28:27 +0200

    acabados hasta funciones

--- main.js
+++ main.js
@@ -113,13 +113,9 @@ const usuario = {
   direccion: {
     ciudad: "Madrid",
   },
-  intereses: ["cocinar","bailar"],
-  trabajos: [ 
-    {empresa: 1},
-    {empresa: "Google"},
-      ],
-}
-
+  intereses: ["cocinar", "bailar"],
+  trabajos: [{ empresa: 1 }, { empresa: "Google" }],
+};
 
 //[OK] // TEST 1
 if (typeof usuario === "object") {
@@ -221,9 +217,6 @@ for (let elemento of arrayNum_3) {
 
 console.log(totalArr_3);
 
-// FOR IN ------------------------------------------------------
-// for in es un metodo de objetos no de arrays
-
 //--------------------------------------------------------------
 
 // Dado un número, encontrar su factorial.
@@ -288,11 +281,39 @@ function imparesOpc2(arr) {
 
 imparesOpc2(array);
 
-//TODO: SEGUIR POR AQUI
+//[OK] // Dado un array de objetos con propiedades "nombre" y "edad", imprimir en la consola solo los nombres de las personas mayores de 18 años.
 
-// Dado un array de objetos con propiedades "nombre" y "edad", imprimir en la consola solo los nombres de las personas mayores de 18 años.
+const personas = [
+  { nombre: "Ana", edad: 20 },
+  { nombre: "Carlos", edad: 25 },
+  { nombre: "María", edad: 16 },
+  { nombre: "Pedro", edad: 30 },
+  { nombre: "Laura", edad: 14 },
+];
 
-// Dado un número, imprimir en la consola si es primo o no.
+personas.forEach((persona) => {
+  if (persona.edad > 18) {
+    console.log(`${persona.nombre} tiene ${persona.edad}`);
+  }
+});
+
+//[OK] // Dado un número, imprimir en la consola si es primo o no.
+
+const numeroPrimo = (num) => {
+  if (num === 0) {
+    return console.log(`${num} no es primo`);
+  }
+
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return console.log(`${num} no es primo`);
+    }
+  }
+
+  return console.log(`${num} es primo`);
+};
+
+numeroPrimo(2);
 
 // Utiliza la siguiente array para resolver los próximos ejercicios:
 
@@ -333,3 +354,95 @@ let animales = [
     edad: 1,
   },
 ];
+
+//[OK] // Crea un array con los animales que son felinos y mayores de 4 años. No utilices el método filter de las arrays y muéstralo por consola.
+
+let felinosMayoresDe4 = [];
+
+animales.forEach((animal) => {
+  if(animal.especie === "Felino" && animal.edad > 4) {
+    console.log(`El ${animal.nombre} es ${animal.especie} y tiene ${animal.edad} años`);
+    felinosMayoresDe4.push(animal)
+  }
+})
+
+console.log(felinosMayoresDe4)
+
+//[OK] // Crea un array con los animales que son herbívoros y tienen una edad par. No utilices el método filter de las arrays y muéstralo por consola.
+
+let herbivorosEdadPar = [];
+
+for (let i = 0; i < animales.length; i++){
+  if(animales[i].especie === "Herbívoro"  && animales[i].edad % 2 === 0){
+    console.log(`${animales[i].nombre} : herbívoro. Edad ${animales[i].edad}`)
+    herbivorosEdadPar.push(animales[i])
+  }
+}
+
+console.log(herbivorosEdadPar)
+
+
+//[OK] // Crea un array con los animales que son omnívoros o aves y menores de 3 años. No utilices el método filter de las arrays y muéstralo por consola.
+
+let omnivorosOavesMenoresDe3 = [];
+
+animales.forEach((animal) => {
+  if (animal.especie === "Omnívoro" || animal.especie === "Ave" && animal.edad < 3 ){
+    omnivorosOavesMenoresDe3.push(animal)
+  }
+})
+
+console.log (omnivorosOavesMenoresDe3)
+
+
+//[OK] // Crea un array con los animales que su especie empieza por "H". No utilices el método filter de las arrays y muéstralo por consola.
+
+let animalesEmpiezanH = [];
+
+animales.forEach((animal) => {
+  if(animal.especie.charAt(0) === "H"){
+    animalesEmpiezanH.push(animal);
+  }
+})
+
+console.log(animalesEmpiezanH)
+
+//[OK] // Crea un array con los animales cuyo nombre tenga más de 4 letras y su especie sea felino o herbívoro. No utilices el método filter de las arrays y muéstralo por consola.
+
+let animalesNombreMasde4LetrasYFelinoOHerbivoro = [];
+
+animales.forEach((animal) => {
+  if(animal.nombre.length > 4 && animal.especie === "Felino" || animal.especie === "Herbívoro" ){
+    animalesNombreMasde4LetrasYFelinoOHerbivoro.push(animal)
+  }
+})
+
+console.log(animalesNombreMasde4LetrasYFelinoOHerbivoro)
+
+//TODO: SEGUIR POR AQUI
+
+/* ----------------------------------------------------
+                    Funciones
+---------------------------------------------------- */
+
+// Crea una función llamada eliminarDuplicados(array) que reciba como parámetro un array de elementos y devuelva un nuevo array con los elementos únicos, es decir, sin elementos duplicados. Puedes utilizar un ciclo for...of para recorrer el array original.
+//salida
+eliminarDuplicados([1, 2, 3, 3, 4, 5, 5, 6]); // Devuelve [1, 2, 3, 4, 5, 6]
+eliminarDuplicados(["hola", "adios", "hola", "hasta", "luego"]); // Devuelve ["hola", "adios", "hasta", "luego"]
+
+// Crea una función llamada sumarElementos(array) que reciba como parámetro un array de números y devuelva la suma de todos sus elementos. Puedes utilizar un ciclo for...of para recorrer el array y sumar sus elementos.
+sumarElementos([1, 2, 3, 4, 5]); // Devuelve 15
+sumarElementos([-5, 10, -15, 20]); // Devuelve 10
+
+// Crea una función en contarCaracteres(array) que reciba como parámetro un array de cadenas de caracteres y devuelva la cantidad total de caracteres que hay en todas las cadenas juntas. Puedes utilizar un ciclo for...of para recorrer el array y contar los caracteres.
+contarCaracteres(["Hola", "Mundo"]); // Devuelve 9
+contarCaracteres(["JavaScript", "es", "genial"]); // Devuelve 18
+
+  // Crea una función llamada numeroMasGrande(array) que reciba como parámetro un array de números y devuelva el número más grande de ese array. Puedes utilizar un ciclo for...of para recorrer el array y condicionales para encontrar el número más grande.
+numeroMasGrande([1, 2, 3, 4, 5]); // Devuelve 5
+numeroMasGrande([10, -5, 20, -15]); // Devuelve 20
+
+
+// Crea una función llamada cadenaMasLarga(array) que reciba como parámetro un array de cadenas de caracteres y devuelva la cadena más larga de ese array. Puedes utilizar un ciclo for...of para recorrer el array y condicionales para encontrar la cadena más larga.
+cadenaMasLarga(["Hola", "Mundo"]); // Devuelve "Mundo"
+cadenaMasLarga(["JavaScript", "es", "genial"]); // Devuelve "JavaScript"
